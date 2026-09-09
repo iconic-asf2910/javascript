d10bb13df915caa352319197a6604e222a4e386b
Iconic <218242901+iconic-asf2910@users.noreply.github.com>
2026-09-09 15:22:22 +0530

    basics

---
+++ 01.basics/05.datatypes  summary.js
@@ -0,0 +1,38 @@
+// primitive
+
+// 7 types : string , number , boolean , null ,, undefined , symbol , bigint
+
+const score = 100
+const scoreValue = 100.3
+
+
+const isLoggendIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id==anotherId)
+
+
+
+
+
+
+// reference (non primitive)
+
+// arraY ,objects , functions
+
+const heros = ["ironman","hulk","natasha"]
+
+let data={
+    name: "hitesh",
+    age: 22,
+
+}
+
+
+const myfunction = function(){
+    console.log("hello world");
+}
