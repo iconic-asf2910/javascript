7c3839ebcd94406f1e489755a5897515a69702e5
Iconic <218242901+iconic-asf2910@users.noreply.github.com>
2026-09-09 11:42:24 +0530

    datatypes

---
+++ 01.basics/02.datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+//alert (3+3) // we are using node.js not browser this works in browser console
+
+console.log(3+3)
+
+console.log("hitesh")
+
+let name = "saksham"
+let age =19
+
+let isLoggedIn = false
+
+let state;
+
+// number
+//bigint
+//string => true/false
+//boolean =>standalone value
+//undefined  =>
+//symbol => unique
+
+
+// object
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof state);//undefined
+console.log(typeof null);//object
