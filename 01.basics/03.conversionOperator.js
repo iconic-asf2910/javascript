0ac3ee4aea236554e28bfc73d3a22a46170ed933
Iconic <218242901+iconic-asf2910@users.noreply.github.com>
2026-09-09 14:59:59 +0530

    conversion2

--- 01.basics/03.conversionOperator.js
+++ 01.basics/03.conversionOperator.js
@@ -31,3 +31,45 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+//********************** operations************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+console.log(2+2);
+
+
+
+let str1 = "hello"
+let str2 = " saksham"
+let str3 = str1 +str2
+console.log(str3);
+
+
+console.log("1" + 2);
+
+console.log(1+"2");
+
+console.log("1"+2+2);
+
+console.log(1+2+"2");
+
+
+console.log(true);
+
+
+let num1, num2 ,num3 
+
+
+num1 = num2 =num3 = 2+2
+
+console.log(num1,num2,num3)
+
+
+let gameCounter = 100
+
+--gameCounter;
+
+console.log(gameCounter);
